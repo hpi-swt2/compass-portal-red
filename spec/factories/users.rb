29cceb0bb4f4df04580ba3eb50d0b74d9fe07467
Christoph Matthies <chrisma@users.noreply.github.com>
2021-11-15 21:15:18 +0100

    Initial user test

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,7 @@
 FactoryBot.define do
   factory :user do
-    
+    sequence(:email) { |n| "user#{n}@example.com" }
+    password { 'password' }
+    password_confirmation { 'password' }
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:user) { FactoryBot.build(:user) } 
+
+  describe "creation using a factory" do
+
+    it "should create a valid object" do
+      expect(user).to be_valid
+    end
+
+    it "should set an email" do
+      expect(user.email).not_to be_blank
+    end
+
+  end
 end
