3aed605b21df28837dd793f218af26d6481dd3db
Christoph Matthies <chrisma@users.noreply.github.com>
2021-11-16 14:07:31 +0100

    A little bit more documentation

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,6 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
+  # Returns a User instance that's not saved
+  # https://github.com/thoughtbot/factory_bot/blob/master/GETTING_STARTED.md#build-strategies
   let(:user) { FactoryBot.build(:user) }
 
   describe "creation using a factory" do
