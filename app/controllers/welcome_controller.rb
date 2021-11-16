e9ff685f6450bdc2db9e9758a10fa44c37251c84
Christoph Matthies <chrisma@users.noreply.github.com>
2021-11-16 20:43:43 +0100

    Add demo flash message

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -9,5 +9,7 @@ class WelcomeController < ApplicationController
 
   def protected
     # Only accessible by logged in users, see `before_action` call
+    # https://guides.rubyonrails.org/action_controller_overview.html#flash-now
+    flash.now[:notice] = "You have reached the protected page! This is a flash message."
   end
 end
