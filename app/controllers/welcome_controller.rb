5bf7a310799c2d42c381705fe8c6a3873d4b1fab
Christoph Matthies <chrisma@users.noreply.github.com>
2021-11-16 14:02:54 +0100

    Add demo login protected page

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,2 +1,13 @@
 class WelcomeController < ApplicationController
+  # https://github.com/heartcombo/devise#controller-filters-and-helpers
+  before_action :authenticate_user!, only: [:protected]
+  # Alternative: before_action :authenticate_user!, :except => [:index]
+
+  def index
+    # Welcome page, accessible without login
+  end
+
+  def protected
+    # Only accessible by logged in users, see `before_action` call
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get "welcome/index"
-  # Defines `root_url`. Devise requires this to be set
+
+  # get 'welcome/index', to: 'welcome#index'
+  get '/protected', to: 'welcome#protected'
+
+  # Defines where '/' routes to, sets `root_url`.
+  # The devise gem requires this to be set
   root to: "welcome#index"
 end
