090fb390228985a76b5ad21b9381937c9badec2d
Kirill Postnov <kirill.postnov@student.hpi.uni-potsdam.de>
2021-11-23 14:13:50 +0100

    Add scaffold for person model

---
+++ spec/factories/people.rb
@@ -0,0 +1,16 @@
+FactoryBot.define do
+  factory :person do
+    name { "MyString" }
+    surname { "MyString" }
+    title { "MyString" }
+    email { "MyString" }
+    phone { "MyString" }
+    office { "MyString" }
+    website { "MyString" }
+    image { "MyString" }
+    chair { "MyString" }
+    office_hours { "MyString" }
+    telegram_handle { "MyString" }
+    knowledge { "MyString" }
+  end
+end
