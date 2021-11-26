2177d4f9fa4571fbe5db1e19b4a265bf5e98a472
Kirill Postnov <kirill.postnov@student.hpi.uni-potsdam.de>
2021-11-26 14:58:53 +0100

    Add tests for HpiWebScraper

---
+++ spec/scraper/hpi_web_scraper_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+require "#{Rails.root}/lib/hpi_web_scraper.rb"
+
+RSpec.describe "HpiWebScraper", type :feature do
+  before :each do
+    @webscraper = HpiWebScraper.new
+  end
+
+  it "extracts surname and name from a person's full name" do
+    name = 'Prof. Holger Giese'
+    name_hash = @webscraper.getNames(name)
+
+    expect(name_hash[:surname].to eq('Holger'))
+    expect(name_hash[:name].to eq('Giese'))
+  end
+  
+end
