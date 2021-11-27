98204c89c91e3984abc4682706dd0d9bdfe6515b
genschelena <Elena.Gensch@student.hpi.uni-potsdam.de>
2021-11-27 13:00:52 +0100

    Add Point of Interest

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_27_110203) do
+ActiveRecord::Schema.define(version: 2021_11_27_113611) do
+
+  create_table "point_of_interests", force: :cascade do |t|
+    t.integer "point_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["point_id"], name: "index_point_of_interests_on_point_id"
+  end
 
   create_table "points", force: :cascade do |t|
     t.float "x"
@@ -34,4 +41,5 @@ ActiveRecord::Schema.define(version: 2021_11_27_110203) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "point_of_interests", "points"
 end
---
+++ spec/models/point_of_interest_spec.rb
@@ -0,0 +1,29 @@
+require 'rails_helper'
+
+RSpec.describe PointOfInterest, type: :model do
+  it "has a constructor that can create point of interest instances" do
+    point_of_interest = described_class.new
+    expect(point_of_interest).to be_an_instance_of(described_class)
+  end
+
+  describe "it should be valid" do
+    it "with a point" do
+      point = build :point
+      point_of_interest = described_class.new(point: point)
+      expect(point_of_interest).to be_valid
+    end
+
+    it "when using the factory" do
+      point_of_interest = build :point_of_interest
+      expect(point_of_interest).to be_valid
+    end
+  end
+
+  describe "it should not be valid" do
+    it "without a point" do
+      point_of_interest = build :point_of_interest
+      point_of_interest.point = nil
+      expect(point_of_interest).not_to be_valid
+    end
+  end
+end
