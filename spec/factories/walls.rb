65bca8ed6a393fb93df2d6899448ec65b664b1c3
genschelena <Elena.Gensch@student.hpi.uni-potsdam.de>
2021-11-28 14:29:57 +0100

    adjust wall model tests

--- spec/factories/walls.rb
+++ spec/factories/walls.rb
@@ -1,5 +1,5 @@
 FactoryBot.define do
   factory :wall do
-    
+    points { [ (build :point), (build :point) ] }
   end
 end
--- spec/models/wall_spec.rb
+++ spec/models/wall_spec.rb
@@ -14,26 +14,35 @@ RSpec.describe Wall, type: :model do
   end
 
   describe "db interaction" do
-    let(:point1) { create :point }
-    let(:point2) { create :point }
+    let(:point1) { create :point, x: 0 }
+    let(:point2) { create :point, x: 0.5 }
+    let(:point3) { create :point, y: 1.0 }
+    let(:points) { [point1, point2, point3] }
     let(:wall) { create :wall }
 
     # change the wall and save it to db
     before do
-      point1.x = 0
-      point2.x = 0.5
-      wall.points = [point1, point2]
+      wall.points = points
       wall.save!
     end
-    
+
+    it "creates valid wall when using the factory" do
+      wall = build :wall, points: points
+      expect(wall).to be_valid
+      expect(wall.points[1].x).to eq(point2.x)
+    end
+
     it "has the wall saved in the db" do
       db_wall = described_class.find(wall.id)
       expect(db_wall).to be_an_instance_of(described_class)
       expect(wall.id).to eq(db_wall.id)
     end
 
-    it "keeps an order of points" do
-      expect(wall.points.size).to eq(2)
+    it "allows adding points to the wall" do
+      expect(wall.points.size).to eq(points.size)
+    end
+
+    it "keeps an order of added points" do
       expect(wall.points[0].x).to eq(point1.x)
       expect(wall.points[1].x).to eq(point2.x)
     end
@@ -44,11 +53,8 @@ RSpec.describe Wall, type: :model do
       wall.save!
       db_wall = described_class.find(wall.id)
 
-
-      expect(db_wall.points.size).to eq(3)
-      expect(db_wall.points[0].id).to eq(point1.id)
-      expect(db_wall.points[1].id).to eq(point2.id)
-      expect(db_wall.points[2].id).to eq(point1.id)
+      expect(db_wall.points.size).to eq(points.size + 1)
+      expect(db_wall.points[points.size].id).to eq(point1.id)
     end
   end
 end
