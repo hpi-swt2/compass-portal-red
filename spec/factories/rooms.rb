ebe624d167c538ab6be6b73a1c37b967389b8946
genschelena <Elena.Gensch@student.hpi.uni-potsdam.de>
2021-11-29 14:41:39 +0100

    fix linting issues (indentation and blocks)

--- spec/factories/rooms.rb
+++ spec/factories/rooms.rb
@@ -1,11 +1,11 @@
 FactoryBot.define do
   factory :room do
-    outerShape {
+    outerShape do
       [(build :point),
        (build :point, x: -1.5),
        (build :point, x: -1.5, y: -1.5),
        (build :point, y: -1.5)]
-    }
+    end
     walls { [(build :wall)] }
   end
 end
