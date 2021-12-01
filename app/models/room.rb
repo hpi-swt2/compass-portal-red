f091d991185f70e23d22940bb986022d6d4f1168
rsommerfeld <romeo.sommerfeld@web.de>
2021-12-01 21:38:07 +0100

    Replace rooms outer_shape point array with polyline

--- app/models/room.rb
+++ app/models/room.rb
@@ -2,7 +2,13 @@ class Room < ApplicationRecord
   # There is a distinction between walls and outer shape. For example, there might be an entrance room connected to
   # a hallway without a door in-between. The outer shape would contain the border between the hallway and entrance room,
   # but there would be no wall segment in the walls array.
-  has_and_belongs_to_many :outer_shape, class_name: "Point"
+  belongs_to :outer_shape, class_name: 'Polyline'
   has_and_belongs_to_many :walls
   has_and_belongs_to_many :point_of_interests
+
+  after_initialize :init
+
+  def init
+    self.outer_shape ||= Polyline.new # if no outer shape exists yet, create an empty one
+  end
 end
