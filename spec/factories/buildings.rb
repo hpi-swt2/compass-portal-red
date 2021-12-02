2cdc9a0aa4f01cd487cb7c33412c584bc3b1a073
genschelena <Elena.Gensch@student.hpi.uni-potsdam.de>
2021-12-02 20:24:01 +0100

    fix linting issues - blocks - room spec - long lines

--- spec/factories/buildings.rb
+++ spec/factories/buildings.rb
@@ -1,5 +1,11 @@
 FactoryBot.define do
   factory :building do
-    rooms { [(build :room), (build :room, point_of_interests: [(build :point_of_interest), (build :point_of_interest, point: (build :point, x: -8.3))])] }
+    rooms do
+      [(build :room),
+       (build :room,
+              point_of_interests:
+                [(build :point_of_interest),
+                 (build :point_of_interest, point: (build :point, x: -8.3))])]
+    end
   end
 end
