a3d1bc739a5095f669b75c4f63278389dfd9aaaf
Nikola Genchev <n.genchev@hotmail.com>
2021-12-02 19:05:43 +0100

    Beta version of live search
    Co-authored-by: Benjamin Frost <Benjamin-Frost@users.noreply.github.com>
    Co-authored-by: LeonHermann322 <leon.hermann@student.hpi.uni-potsdam.de>

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,5 +1,20 @@
 class SearchController < ApplicationController
-  def index
+  #def index
     # Search page, accessible without login
+  #end
+
+  def index
+    # basic array to test live search functionality
+    # will be changed later when an actual DB exists
+    arr = ['Daniel', 'Djamal', 'Dan']
+    if (params[:query] == nil) 
+      return 
+    end
+    @query = arr.select{|e| e.include?(params[:query])}
+    #print('================================')
+    #print(@query)
+    return @query
   end
+
+  helper_method :index
 end
--- spec/features/search/access_search_page_spec.rb
+++ spec/features/search/access_search_page_spec.rb
@@ -10,8 +10,17 @@ RSpec.describe "Search Page", type: :feature do
     expect(page).to have_link(nil, href: search_path)
   end
 
-  it "it contains a search field" do
+  it "contains a search field" do
     visit search_path
     expect(page).to have_field("search")
+  end
+
+  it "should render a list when @query is not empty" do 
+    # This test will fail once the method in the controller is changed or the database changes
+    visit search_path + '?query=D&commit=Search'
+    expect(page).to have_link 'Dan'
+    expect(page).to have_link 'Djamal'
+    expect(page).to have_link 'Daniel'
+
   end
 end
