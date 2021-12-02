1be69c9adc37d88baa35a6a21d42dbf8ae2f6503
genschelena <Elena.Gensch@student.hpi.uni-potsdam.de>
2021-12-02 23:01:50 +0100

    fix linting issues - class comments - tests

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,8 +1,7 @@
+# There is a distinction between walls and outer shape. For example, there might be an entrance room connected to
+# a hallway without a door in-between. The outer shape would contain the border between the hallway and entrance room,
+# but there would be no wall segment in the walls array.
 class Room < ApplicationRecord
-  # There is a distinction between walls and outer shape. For example, there might be an entrance room connected to
-  # a hallway without a door in-between. The outer shape would contain the border between the hallway and entrance room,
-  # but there would be no wall segment in the walls array.
-
   belongs_to :building, optional: true # optional, because rooms should be creatable without creating a building
   belongs_to :outer_shape, class_name: 'Polyline'
   has_and_belongs_to_many :walls
