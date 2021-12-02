5f1bc980ec00b0bb62ee7de2207399727bb0cb48
genschelena <Elena.Gensch@student.hpi.uni-potsdam.de>
2021-12-02 23:01:50 +0100

    fix linting issues - class comments - tests

--- app/models/wall.rb
+++ app/models/wall.rb
@@ -1,6 +1,6 @@
+# A wall containing points (A, B, C) has 2 segments (a, b) and (b, c).
+# If you want to make a circle, you should add the wall as (A, B, C, A).
 class Wall < ApplicationRecord
-  # A wall containing points (A, B, C) has 2 segments (a, b) and (b, c)
-  # If you want to make a circle, you should add the wall as (A, B, C, A).
   belongs_to :polyline
   after_initialize :init
 
