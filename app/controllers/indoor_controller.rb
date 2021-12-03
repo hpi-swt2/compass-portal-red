59f8629e3f6b5504f34696ffffd807bf36c37840
AbdullatifGhajar <abdullatif.ghajar@gmail.com>
2021-12-03 09:45:26 +0100

    Rebase from model branch

--- app/controllers/indoor_controller.rb
+++ app/controllers/indoor_controller.rb
@@ -14,4 +14,20 @@ class IndoorController < ApplicationController
             ]) ]
         )
     end
+    include IndoorHelper
+    def geojson
+        render json: Room.new(
+            outerShape: [
+                Point.new(x: 30, y: 80),
+                Point.new(x: 30, y: 30),
+                Point.new(x: 80, y: 30),
+                Point.new(x: 80, y: 80)
+            ],
+            point_of_interests: [PointOfInterest.new(point: Point.new(x: 20, y: 50))],
+            walls: [ Wall.new(points: [
+                Point.new(x: 30, y: 30),
+                Point.new(x: 80, y: 80)
+            ]) ]
+        ).to_geojson
+    end
 end
