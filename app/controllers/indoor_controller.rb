b4659765aab69a22f4b1652a6b6c05c8c633ae66
AbdullatifGhajar <abdullatif.ghajar@gmail.com>
2021-12-03 09:46:32 +0100

    Integrate new models

--- app/controllers/indoor_controller.rb
+++ app/controllers/indoor_controller.rb
@@ -1,33 +1,33 @@
 class IndoorController < ApplicationController
     def demo
-        @room = Room.new(
-            outerShape: [
+        @building = Building.new(rooms: [Room.new(
+            outer_shape: Polyline.new(points: [
                 Point.new(x: 30, y: 80),
                 Point.new(x: 30, y: 30),
                 Point.new(x: 80, y: 30),
                 Point.new(x: 80, y: 80)
-            ],
+            ]),
             point_of_interests: [PointOfInterest.new(point: Point.new(x: 20, y: 50))],
-            walls: [ Wall.new(points: [
+            walls: [ Wall.new(polyline: Polyline.new(points: [
                 Point.new(x: 30, y: 30),
                 Point.new(x: 80, y: 80)
-            ]) ]
-        )
+            ])) ]
+        )])
     end
     include IndoorHelper
     def geojson
-        render json: Room.new(
-            outerShape: [
+        render json: Building.new(rooms: [Room.new(
+            outer_shape: Polyline.new(points: [
                 Point.new(x: 30, y: 80),
                 Point.new(x: 30, y: 30),
                 Point.new(x: 80, y: 30),
                 Point.new(x: 80, y: 80)
-            ],
+            ]),
             point_of_interests: [PointOfInterest.new(point: Point.new(x: 20, y: 50))],
-            walls: [ Wall.new(points: [
+            walls: [ Wall.new(polyline: Polyline.new(points: [
                 Point.new(x: 30, y: 30),
                 Point.new(x: 80, y: 80)
-            ]) ]
-        ).to_geojson
+            ])) ]
+        )]).to_geojson
     end
 end
