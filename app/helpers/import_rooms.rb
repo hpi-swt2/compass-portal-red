6b048fa883d9523824518db8779a4aada7b1c68f
genschelena <Elena.Gensch@student.hpi.uni-potsdam.de>
2021-12-06 14:52:11 +0100

    fix linting issues

--- app/helpers/import_rooms.rb
+++ app/helpers/import_rooms.rb
@@ -4,7 +4,7 @@ require 'room'
 require 'building'
 require 'polyline'
 
-rooms = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I']
+rooms = %w[A B C D E F G H I]
 building = Building.create
 
 csv_text = File.read('app/assets/data/importBuilding1.csv')
@@ -13,15 +13,15 @@ points = {}
 rooms.each do |name|
   polygon = {}
   csv.each do |row|
-    if row[0..3].collect { |x| x.to_s[0] }.include? name.to_s
-      key = row[0].to_s + row[1].to_s + row[2].to_s + row[3].to_s
-      id = row[0..3].find { |e| e.to_s[0] == name.to_s }
-      number = id[1..id.length - 1] # will not work if more than 24 rooms (can be fixed with regex)
-      points[key] = Point.create(x: row[4], y: row[5]) unless points.key? key
-      polygon[number] = points[key]
-    end
+    next unless row[0..3].collect { |x| x.to_s[0] }.include? name.to_s
+
+    key = row[0].to_s + row[1].to_s + row[2].to_s + row[3].to_s
+    id = row[0..3].find { |e| e.to_s[0] == name.to_s }
+    number = id[1..id.length - 1] # will not work if more than 24 rooms (can be fixed with regex)
+    points[key] = Point.create(x: row[4], y: row[5]) unless points.key? key
+    polygon[number] = points[key]
   end
-  polygon = polygon.sort_by { |key, value| key.to_i }.to_h
+  polygon = polygon.sort_by { |key, _value| key.to_i }.to_h
   room = Room.new(building: building)
   room.outer_shape = Polyline.create
   room.outer_shape.points.push(polygon.values)
