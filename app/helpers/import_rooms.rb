80d2d5eba3fe52341498200d3f6b5fc2c00109c1
smilla.fox <smilla.fox@student.hpi.uni-potsdam.de>
2021-12-06 14:12:32 +0100

    Correct linting issues Co-authored-by: MartinPreiss <MartinPreiss@users.noreply.github.com>

--- app/helpers/import_rooms.rb
+++ app/helpers/import_rooms.rb
@@ -4,33 +4,27 @@ require 'room'
 require 'building'
 require 'polyline'
 
-rooms = ['A','B','C','D','E','F','G','H','I']
+rooms = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I']
 building = Building.create
 
 csv_text = File.read('app/assets/data/importBuilding1.csv')
 csv = CSV.parse(csv_text)
-points = Hash.new
-rooms.each do |name, array|
-    polygon = Hash.new
-    csv.each do |row|
-        if row[0..3].collect{|x| x.to_s[0] }.include? name.to_s
-            key = row[0].to_s + row[1].to_s + row[2].to_s + row[3].to_s
-            id = row[0..3].select{|e| e.to_s[0] == name.to_s}[0]
-            number = id[1..id.length-1] #will not work if more than 24 rooms (can be fixed with regex)
-            if points.key? key
-                polygon[number] = points[key]
-            else
-                points[key] = Point.create(x: row[4], y: row[5])
-                polygon[number] = points[key]
-            end
-        end
+points = {}
+rooms.each do |name|
+  polygon = {}
+  csv.each do |row|
+    if row[0..3].collect { |x| x.to_s[0] }.include? name.to_s
+      key = row[0].to_s + row[1].to_s + row[2].to_s + row[3].to_s
+      id = row[0..3].find { |e| e.to_s[0] == name.to_s }
+      number = id[1..id.length - 1] # will not work if more than 24 rooms (can be fixed with regex)
+      points[key] = Point.create(x: row[4], y: row[5]) unless points.key? key
+      polygon[number] = points[key]
     end
-    polygon = Hash[polygon.sort_by{ |key, value| key.to_i }]
-    room = Room.new(building: building)
-    room.outer_shape = Polyline.create
-    room.outer_shape.points.push(polygon.values)
-    room.outer_shape.points.push(polygon['1'])
-    room.save
+  end
+  polygon = polygon.sort_by { |key, value| key.to_i }.to_h
+  room = Room.new(building: building)
+  room.outer_shape = Polyline.create
+  room.outer_shape.points.push(polygon.values)
+  room.outer_shape.points.push(polygon['1'])
+  room.save
 end
-
-
