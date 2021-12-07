b621ef3785c7075cbac223509e10d0a4f8fe3275
Simon Cyrani <simon.cyrani@student.hpi.de>
2021-12-07 20:12:33 +0100

    feat: added remaining dummy tables for BR - room, room_type, tag

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,7 +4,7 @@
 person_list = [
   [ "michael.perscheid@hpi.de", "Michael", "Perscheid", "Dr.", "https://via.placeholder.com/150",
     "Chair Representative" ],
-  [ "hasso.plattner@hpi.de", "Hasso", "Plattner", "Prof. Dr.", "https://via.placeholder.com/150", "Professor" ],
+  [ "hasso.plattner@hpi.de", "Hasso", "Plattner", "Prof. Dr. h.c.", "https://via.placeholder.com/150", "Professor" ],
   [ "mr.net@hpi.de", "Mr.", "Net", "", "https://via.placeholder.com/150", "" ],
   [ "morpheus@student.hpi.de", "Morpheus", "Cyrani", "", "https://via.placeholder.com/150", "Tutor" ],
   [ "biene.maya@kika.de", "Maya", "Biene", "", "https://via.placeholder.com/150", "Extern" ]
@@ -16,35 +16,61 @@ chair_list = [
   [ "Human Computer Interaction" ],
   [ "Internet-Technologien und Systeme" ]
 ]
+information_list = [
+    [ "Telegram", "@perscheid" ],
+    [ "Telegram", "@plattner" ],
+    [ "Slack", "@mr.net" ],
+    [ "Signal", "@morpheus" ],
+    [ "Website", "diebienemaya.de" ],
+]
+room_list = [
+    [ "V-2.18", "2", "Campus II (Villa), V-2.18"],
+    [ "V-2.12", "2", "Campus II (Villa), V-2.18"],
+    [ "H-E.51", "E", "Campus I, H-E.51"],
+    [ "H-2.3", "2", "Bachelorprojekt Baudisch"],
+    [ "A-1.15", "1", "A-1.15"],
+]
+room_type_list = [
+    ["Seminarraum" ],
+    ["Hörsaal" ],
+    ["Büro" ],
+    ["Toilette" ],
+    ["Bachelorprojekt"],
+]
 person_collection = []
 
 person_list.each do |email, first_name, last_name, title, image, status|
     person_collection << Person.create(email: email, first_name: first_name, last_name: last_name, title: title, image: image,
         status: status)
     end
-    
-chair_list = [
-    [ "Enterprise Platform and Integration Concepts" ],
-    [ "Algorithm Engineering" ],
-    [ "Data Science and Computational Statistics" ],
-    [ "Human Computer Interaction" ],
-    [ "Internet-Technologien und Systeme" ]
-]
+
 bundle = person_collection.zip chair_list
 
 bundle.each do |person, chair|
   chair = Chair.create(name: chair, people: [person])
 end
     
-information_list = [
-    [ "Telegram", "@perscheid" ],
-    [ "Telegram", "@plattner" ],
-    [ "Slack", "@mr.net" ],
-    [ "Signal", "@morpheus" ],
-    [ "Website", "die-biene-maya.de" ],
-]
 bundle = person_collection.zip information_list
 
 bundle.each do |person, information|
     Information.create(key: information[0], value: information[1], person: person)
 end
+
+
+bundle = person_collection.zip room_list
+
+bundle.each do |person, room|
+    Room.create(number: room[0], floor: room[1], full_name: room[2], people: [person])
+end
+
+room_type_list.each do |name|
+    RoomType.create(name: name)
+end
+
+Tag.create(name:"Seminarraum", rooms: [Room.find(5), Room.find(3)])
+Tag.create(name:"Drucker", rooms: [Room.find(4)])
+Room.find(1).room_types << RoomType.find(3)
+Room.find(2).room_types << RoomType.find(3)
+Room.find(3).room_types << RoomType.find(1)
+Room.find(4).room_types << RoomType.find(5)
+Room.find(5).room_types << RoomType.find(1)
