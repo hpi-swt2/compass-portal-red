19016926e53da5335ab5c29d2b603ec56c01368b
Simon Cyrani <simon.cyrani@student.hpi.de>
2021-12-07 20:15:10 +0100

    refactor: autolint

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,58 +17,57 @@ chair_list = [
   [ "Internet-Technologien und Systeme" ]
 ]
 information_list = [
-    [ "Telegram", "@perscheid" ],
-    [ "Telegram", "@plattner" ],
-    [ "Slack", "@mr.net" ],
-    [ "Signal", "@morpheus" ],
-    [ "Website", "diebienemaya.de" ],
+  [ "Telegram", "@perscheid" ],
+  [ "Telegram", "@plattner" ],
+  [ "Slack", "@mr.net" ],
+  [ "Signal", "@morpheus" ],
+  [ "Website", "diebienemaya.de" ]
 ]
 room_list = [
-    [ "V-2.18", "2", "Campus II (Villa), V-2.18"],
-    [ "V-2.12", "2", "Campus II (Villa), V-2.18"],
-    [ "H-E.51", "E", "Campus I, H-E.51"],
-    [ "H-2.3", "2", "Bachelorprojekt Baudisch"],
-    [ "A-1.15", "1", "A-1.15"],
+  [ "V-2.18", "2", "Campus II (Villa), V-2.18"],
+  [ "V-2.12", "2", "Campus II (Villa), V-2.18"],
+  [ "H-E.51", "E", "Campus I, H-E.51"],
+  [ "H-2.3", "2", "Bachelorprojekt Baudisch"],
+  [ "A-1.15", "1", "A-1.15"]
 ]
 room_type_list = [
-    ["Seminarraum" ],
-    ["Hörsaal" ],
-    ["Büro" ],
-    ["Toilette" ],
-    ["Bachelorprojekt"],
+  ["Seminarraum" ],
+  ["Hörsaal" ],
+  ["Büro" ],
+  ["Toilette" ],
+  ["Bachelorprojekt"]
 ]
 person_collection = []
 
 person_list.each do |email, first_name, last_name, title, image, status|
-    person_collection << Person.create(email: email, first_name: first_name, last_name: last_name, title: title, image: image,
-        status: status)
-    end
+  person_collection << Person.create(email: email, first_name: first_name, last_name: last_name, title: title, image: image,
+                                     status: status)
+end
 
 bundle = person_collection.zip chair_list
 
 bundle.each do |person, chair|
   chair = Chair.create(name: chair, people: [person])
 end
-    
+
 bundle = person_collection.zip information_list
 
 bundle.each do |person, information|
-    Information.create(key: information[0], value: information[1], person: person)
+  Information.create(key: information[0], value: information[1], person: person)
 end
-
 
 bundle = person_collection.zip room_list
 
 bundle.each do |person, room|
-    Room.create(number: room[0], floor: room[1], full_name: room[2], people: [person])
+  Room.create(number: room[0], floor: room[1], full_name: room[2], people: [person])
 end
 
 room_type_list.each do |name|
-    RoomType.create(name: name)
+  RoomType.create(name: name)
 end
 
-Tag.create(name:"Seminarraum", rooms: [Room.find(5), Room.find(3)])
-Tag.create(name:"Drucker", rooms: [Room.find(4)])
+Tag.create(name: "Seminarraum", rooms: [Room.find(5), Room.find(3)])
+Tag.create(name: "Drucker", rooms: [Room.find(4)])
 Room.find(1).room_types << RoomType.find(3)
 Room.find(2).room_types << RoomType.find(3)
 Room.find(3).room_types << RoomType.find(1)
