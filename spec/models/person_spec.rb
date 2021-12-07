eacbe06caa1aec9c6386d9333d5ee15163a67f89
Lukas Wenner <lukas.wenner@web.de>
2021-12-07 17:03:51 +0100

    refactor: used before in person to reduce code dup

--- spec/models/person_spec.rb
+++ spec/models/person_spec.rb
@@ -1,6 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Person, type: :model do
+
+  before do
+    @person = FactoryBot.create :person
+  end
+
   it "returns not valid because of empty fields" do
     person = described_class.new(last_name: "Perscheid")
     expect(person).not_to be_valid
@@ -14,35 +19,29 @@ RSpec.describe Person, type: :model do
   end
 
   it "has a information relation" do
-    person = FactoryBot.create :person
-    expect(person.informations.length).to eq(0)
+    expect(@person.informations.length).to eq(0)
   end
 
   it "has a room relation" do
-    person = FactoryBot.create :person
-    person.create_room(full_name: "HS1")
-    expect(person.room.full_name).to eq("HS1")
+    @person.create_room(full_name: "HS1")
+    expect(@person.room.full_name).to eq("HS1")
   end
 
   it "has a chair relation" do
-    person = FactoryBot.create :person
-    expect(person.chairs.length).to eq(0)
+    expect(@person.chairs.length).to eq(0)
   end
 
   it "has a user relation" do
-    person = FactoryBot.create :person
     user = FactoryBot.create :user
-    person.user = user
-    expect(person.user.id).to eq(user.id)
+    @person.user = user
+    expect(@person.user.id).to eq(user.id)
   end
 
   it "calculates its name correctly" do
-    person = FactoryBot.create :person
-    expect(person.name).to eq("#{person.first_name} #{person.last_name}")
+    expect(@person.name).to eq("#{@person.first_name} #{@person.last_name}")
   end
 
   it "calculates its full name correctly" do
-    person = FactoryBot.create :person
-    expect(person.full_name).to eq("#{person.title} #{person.first_name} #{person.last_name}")
+    expect(@person.full_name).to eq("#{@person.title} #{@person.first_name} #{@person.last_name}")
   end
 end
