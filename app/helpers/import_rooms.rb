bda2a826c2633ad02642b9bd042b85e01e3796e9
MartinPreiss <68462093+MartinPreiss@users.noreply.github.com>
2021-12-07 14:01:48 +0100

    update comments

--- app/helpers/import_rooms.rb
+++ app/helpers/import_rooms.rb
@@ -5,6 +5,7 @@ require 'building'
 require 'polyline'
 
 rooms = %w[A B C D E F G H I]
+# extend to import more rooms 
 building = Building.create
 
 csv_text = File.read('app/assets/data/importBuilding1.csv')
@@ -12,12 +13,13 @@ csv = CSV.parse(csv_text)
 points = {}
 rooms.each do |name|
   polygon = {}
+  # Collect all points of this room
   csv.each do |row|
     next unless row[0..3].collect { |x| x.to_s[0] }.include? name.to_s
 
     key = row[0].to_s + row[1].to_s + row[2].to_s + row[3].to_s
     id = row[0..3].find { |e| e.to_s[0] == name.to_s }
-    number = id[1..id.length - 1] # will not work if more than 24 rooms (can be fixed with regex)
+    number = id[1..id.length - 1] # will not work with multi-char room identifier
     points[key] = Point.create(x: row[4], y: row[5]) unless points.key? key
     polygon[number] = points[key]
   end
