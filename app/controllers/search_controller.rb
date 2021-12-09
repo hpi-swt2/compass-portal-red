b919c40056498af925f3e28b484f569d012d0ada
Nikola Genchev <n.genchev@hotmail.com>
2021-12-09 12:32:41 +0100

    Fixing some linting errors

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,19 +1,18 @@
 class SearchController < ApplicationController
-  
   def index
     arr = []
     return if params[:query].nil?
-    
+
     arr = Person.where("first_name like ? or last_name like ?", "%#{params[:query]}%", "%#{params[:query]}%")
     @query = []
     arr.each { |el| @query.push(el.full_name) }
-    
+
     arr = Room.where("full_name like ?", "%#{params[:query]}%")
     arr.each { |el| @query.push("Room: " + el.full_name) }
-    
+
     arr = Chair.where("name like ?", "%#{params[:query]}%")
-    arr.each { |el| @query.push("Chair: " + el.name)}
-    
+    arr.each { |el| @query.push("Chair: " + el.name) }
+
     @params = params[:query]
   end
 
