654a15b585e737ea00b99edd7d58cecb32281ca3
DevSchmidtchen <contact@schmidtchen.me>
2021-12-09 12:52:49 +0100

    hotfix

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,6 +1,5 @@
 class SearchController < ApplicationController
   def index
-    
     return if params[:query].nil?
 
     arr = Person.where("first_name like ? or last_name like ?", "%#{params[:query]}%", "%#{params[:query]}%")
@@ -8,10 +7,10 @@ class SearchController < ApplicationController
     arr.each { |el| @query.push(el.full_name) }
 
     arr = Room.where("full_name like ?", "%#{params[:query]}%")
-    arr.each { |el| @query.push("Room: #{el.full_name)}" }
+    arr.each { |el| @query.push("Room: #{el.full_name}") }
 
     arr = Chair.where("name like ?", "%#{params[:query]}%")
-    arr.each { |el| @query.push("Chair: #{el.name)}" }
+    arr.each { |el| @query.push("Chair: #{el.name}") }
 
     @params = params[:query]
   end
