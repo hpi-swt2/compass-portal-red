b45e11c87ccfe362cb8bbf5647b5978b714ec48f
genschelena <Elena.Gensch@student.hpi.uni-potsdam.de>
2021-12-11 18:58:56 +0100

    Import pipeline for gpx files (label for the geographical location of rooms) Persists - Points, Rooms (and Polyline for outershape), Building

--- app/helpers/import_gpx.rb
+++ app/helpers/import_gpx.rb
@@ -1,27 +1,15 @@
 require 'csv'
-require 'point'
-require 'room'
+require 'indoor_helper'
+require 'optparse'
 require 'building'
-require 'polyline'
 require 'nokogiri'
 
-doc = Nokogiri::XML(File.open('app/assets/data/importBuilding1.gpx'))
+include IndoorHelper
+
+doc = Nokogiri::XML(File.open("app/assets/data/#{ARGV.first}"))
 rooms = doc.css("//trk")
+building = Building.create
 
 rooms.each do |room_node|
-  name_node = room_node.css("name").first
-  name = name_node.content
-
-  point_nodes = room_node.css("trkpt")
-  points = []
-
-  point_nodes.each do |point_node|
-    lat = point_node["lat"]
-    lon = point_node["lon"]
-
-    print("Lat: " + lat + " Lon: " + lon + "\n")
-    points.push([lat, lon])
-  end
-
-  # TODO: do sth with the room name and the points :D
+  build_room_from(room_node, building)
 end
