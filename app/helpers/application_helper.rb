5f4f34fc51717b44910421239f2bd538ac50b727
Kirill Postnov <kirill.postnov@student.hpi.uni-potsdam.de>
2021-12-15 16:02:57 +0100

    Move and rewrite people helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+    def form_color(key, params)
+        if params.include? key
+            return "#FF9999"
+        end
+        return "#FFFFFF"
+    end
 end
--- app/helpers/people_helper.rb
+++ app/helpers/people_helper.rb
@@ -1,10 +1,2 @@
 module PeopleHelper
-    def form_color(key, params)
-        for param in params do
-            if param == key
-                return "#FF9999"
-            end
-        end
-        return "#FFFFFF"
-    end
 end
