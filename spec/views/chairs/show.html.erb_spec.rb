e201174b4986920ac61a9ebc3c7a8854ccf15d68
cdfhalle <conradhalle@gmail.com>
2021-12-17 09:27:09 +0100

    test: added tests for chair detail page

--- spec/views/chairs/show.html.erb_spec.rb
+++ spec/views/chairs/show.html.erb_spec.rb
@@ -4,9 +4,15 @@ RSpec.describe "chairs/show", type: :view do
   before do
     @chair = FactoryBot.create :chair
   end
-
   it "renders attributes in <p>" do
     render
     expect(rendered).to match(@chair.name)
+
+  end
+  it "renders a list of the people that use the room" do
+    render
+    @room.people.each do |person|
+      expect(rendered).to have_link(person.name, href: person_path(person))
+    end
   end
 end
