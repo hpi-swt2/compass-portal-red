9a32fe0c147c13bd435ab3bb1ff61f4e2b6ce8ee
Franz Sauerwald <15076254+FranzSw@users.noreply.github.com>
2021-12-17 01:39:13 +0100

    fix: lint ApplicationHelper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,8 +1,8 @@
 module ApplicationHelper
-    def check_human_verification(datetime)
-        return false if !datetime
+  def check_human_verification(datetime: Time)
+    return false unless datetime
 
-        # Return false once the last human verification was more than 180 days ago
-        (Time.now - datetime) / (24 * 60 * 60) < 180
-    end
+    # Return false once the last human verification was more than 180 days ago
+    (Time.now.utc - datetime.utc) / (24 * 60 * 60) < 180
+  end
 end
