efaa4a51edc098b230686387f210f263173af21e
Kirill Postnov <kirill.postnov@student.hpi.uni-potsdam.de>
2021-12-31 23:57:16 +0100

    Add further scraper tests

--- spec/scraper/scraper_spec.rb
+++ spec/scraper/scraper_spec.rb
@@ -2,47 +2,62 @@ require 'rails_helper'
 require './lib/scraping/scraper'
 
 RSpec.describe Scraper, type: :feature do
-
   let(:base_url) { "#{Rails.root}/spec/scraper" }
+  let(:scrape) { described_class.scrape(base_url) }
 
   before do
     PersonUrl.delete_all
-    person_url = FactoryBot.build :person_url
-    person_url.save
+    Person.delete_all
+
+    FactoryBot.create :person_url
   end
 
-  context "Person database is empty" do
+  it "adds a maximum of one database entry after scraping" do
+    previous_number_entries = Person.count
+    scrape
 
-    it "adds a maximum of one database entry after scraping" do
-        previous_number_entries = Person.count
-        described_class.scrape(base_url)
+    expect(Person.count).to be <= previous_number_entries + 1
+  end
 
-        expect(Person.count).to be <= previous_number_entries + 1
-    end
+  it "scrapes an HPI webpage and stores the peoples' data in the database" do
+    scrape
 
-    it "scrapes an HPI webpage and stores the peoples' data in the database" do
-        described_class.scrape(base_url)
-        
-        expect(Person.all.first).to have_attributes(
-            :first_name => "Michael",
-            :last_name => "Perscheid",
-            :email => 'michael.perscheid(at)hpi.de',
-            :image => '/assets/images/people/Michael_Perscheid_Portrait_120x160.jpg'
-        )
-    end
+    expect(Person.first).to have_attributes(
+      first_name: "Michael",
+      last_name: "Perscheid",
+      email: 'michael.perscheid(at)hpi.de',
+      image: '/assets/images/people/Michael_Perscheid_Portrait_120x160.jpg'
+    )
+  end
 
-    it "doesn't overwrite data if new data is found" do
-        FactoryBot.create :person_url, url: '/html_mocks/table_overwrite.html'
-        described_class.scrape(base_url)
-        
-        expect(Person.all.first).to have_attributes(
-            :first_name => "Michael",
-            :last_name => "Perscheid",
-            :email => 'michael.perscheid(at)hpi.de',
-            :image => '/assets/images/people/Michael_Perscheid_Portrait_120x160.jpg'
-        )
-    end
+  it "doesn't overwrite data if new data is found" do
+    FactoryBot.create :person, email: 'michael.perscheid(at)hpi.de',
+                               image: '/assets/images/people/Michael_Perscheid_Portrait_120x160.jpg'
+    FactoryBot.create :person_url, url: '/html_mocks/table_overwrite.html'
 
+    scrape
+
+    expect(Person.first).to have_attributes(
+      first_name: "Michael",
+      last_name: "Perscheid",
+      email: 'michael.perscheid(at)hpi.de',
+      image: '/assets/images/people/Michael_Perscheid_Portrait_120x160.jpg'
+    )
+  end
+
+  it "stores a room after scraping peoples' data and references it" do
+    scrape
+
+    scraped_person = Person.first
+
+    expect(Room.where(id: scraped_person.room_id, number: 'Campus II (Villa), V-2.18')).to exist
+  end
+
+  it "stores additional information after scraping" do
+    scrape
+
+    expect(Information.where(key: 'phone', value: '+49 (331) 5509-566')).to exist
+    expect(Information.where(key: 'website', value: "#{base_url}/html_mocks/table.html")).to exist
   end
 
 end
