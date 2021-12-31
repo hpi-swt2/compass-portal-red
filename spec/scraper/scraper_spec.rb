56853ab6a4fd876a0d9300f4dfae1d8e0b5ea7b2
Kirill Postnov <kirill.postnov@student.hpi.uni-potsdam.de>
2021-12-31 17:51:45 +0100

    Add tests for scraper

--- spec/scraper/scraper_spec.rb
+++ spec/scraper/scraper_spec.rb
@@ -7,26 +7,40 @@ RSpec.describe Scraper, type: :feature do
 
   before do
     PersonUrl.delete_all
-
-    person_url = FactoryBot.build :person_url, name: 'Dr. Michael Perscheid', url: '/html_mocks/table.html'
+    person_url = FactoryBot.build :person_url
     person_url.save
   end
 
   context "Person database is empty" do
 
-    it "adds a database entry after scraping" do
+    it "adds a maximum of one database entry after scraping" do
         previous_number_entries = Person.count
         described_class.scrape(base_url)
 
-        expect(Person.count).to eq(previous_number_entries + 1)
+        expect(Person.count).to be <= previous_number_entries + 1
     end
 
     it "scrapes an HPI webpage and stores the peoples' data in the database" do
-
+        described_class.scrape(base_url)
+        
+        expect(Person.all.first).to have_attributes(
+            :first_name => "Michael",
+            :last_name => "Perscheid",
+            :email => 'michael.perscheid(at)hpi.de',
+            :image => '/assets/images/people/Michael_Perscheid_Portrait_120x160.jpg'
+        )
     end
 
-    it "doesn't overwrite data if it already exists" do
-
+    it "doesn't overwrite data if new data is found" do
+        FactoryBot.create :person_url, url: '/html_mocks/table_overwrite.html'
+        described_class.scrape(base_url)
+        
+        expect(Person.all.first).to have_attributes(
+            :first_name => "Michael",
+            :last_name => "Perscheid",
+            :email => 'michael.perscheid(at)hpi.de',
+            :image => '/assets/images/people/Michael_Perscheid_Portrait_120x160.jpg'
+        )
     end
 
   end
