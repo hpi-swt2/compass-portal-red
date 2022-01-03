435915aab006cb4e5c11aacd02d2414bd263e195
Kirill Postnov <kirill.postnov@student.hpi.uni-potsdam.de>
2022-01-03 16:13:03 +0100

    Refine tests

--- spec/mailers/email_reminder_spec.rb
+++ spec/mailers/email_reminder_spec.rb
@@ -3,27 +3,28 @@ require "./lib/mailing/email_reminder"
 require "./lib/mailing/email_sender"
 RSpec.describe EmailReminder, type: :mailer do
 
+  let(:mock_sender) { class_double("EmailSender").as_stubbed_const(transfer_nested_constants: true) }
+  let(:person) { FactoryBot.create :person, first_name: 'Max', last_name: 'Mustermann', email: 'email@example.com' }
+
+  before(:each) do
+    DataProblem.delete_all
+  end
+
   context 'when sending out data problem reminder emails' do
-    it 'sends an email when there is a data problem conceirning a person that has not received an email recently' do
-      mock_sender = class_double("EmailSender").as_stubbed_const(transfer_nested_constants: true)
-      person = Person.create!(first_name: 'Max', last_name: 'Mustermann', email: 'email@example.com')
-      DataProblem.delete_all
-      problem = DataProblem.create!(people_id: person.id, description: "some Problem")
+    it 'sends an email when there is a data problem concerning a person that has not received an email recently' do 
+      problem = DataProblem.create!(person_id: person.id, description: "some Problem")
       expect(mock_sender).to receive(:send_email).with(person, [problem])
 
       described_class.remind(mock_sender)
-      loggedEmails = EmailLog.where(people_id: person.id)
+      loggedEmails = EmailLog.where(person_id: person.id)
       assert(loggedEmails.size == 1)
       assert(loggedEmails[0].email_address == person.email)
 
     end
 
-    it 'does not send an email when there is a data problem conceirning a person that has recently received an email recently' do
-      mock_sender = class_double("EmailSender").as_stubbed_const(transfer_nested_constants: true)
-      person = Person.create!(first_name: 'Max', last_name: 'Mustermann', email: 'email@example.com')
-      DataProblem.delete_all
+    it 'does not send an email when there is a data problem concerning a person that has recently received an email recently' do
       EmailLog.create!(email_address: person.email, last_sent: Date.current, people_id: person.id)
-      data_problems = [DataProblem.create!(people_id: person.id, description: "some Problem")]
+      data_problems = [DataProblem.create!(person_id: person.id, description: "some Problem")]
       expect(mock_sender).not_to receive(:send_email).with(person, data_problems)
       described_class.remind(mock_sender)
 
