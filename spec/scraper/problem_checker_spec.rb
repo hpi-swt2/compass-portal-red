b7000006be918870d360609011222f6bdd37caba
Alex <grepolisiert@hotmail.de>
2022-01-03 22:51:20 +0000

    complete unit tests for problem checker

--- spec/scraper/problem_checker_spec.rb
+++ spec/scraper/problem_checker_spec.rb
@@ -14,15 +14,6 @@ RSpec.describe "ProblemChecker", type: :feature do
     expect(data_problems.length).to eq 1
   end
 
-  it "checks if information for a models entry is in conflict to the entries actual content" do
-    check = problem_checker.check_for_conflict(person, 'first_name')
-    expect(check).to be true
-
-    person.update first_name: nil
-    check = problem_checker.check_for_conflict(person, 'first_name')
-    expect(check).to be false
-  end
-
   it "saves a 'outdated' problem for every outdated entry of a model" do
     person.update updated_at: DateTime.now.days_ago(184)
 
@@ -30,6 +21,35 @@ RSpec.describe "ProblemChecker", type: :feature do
 
     data_problems = DataProblem.where(["description = 'outdated'"])
     expect(data_problems).not_to be_empty
+  end
+
+  context "when checking for conflicts" do
+    it "checks if new gathered personal data is in conflict to the actual content of this persons entry" do
+      check = problem_checker.check_for_conflict(person, 'first_name')
+      expect(check).to be true
+
+      person.update first_name: nil
+      check_new = problem_checker.check_for_conflict(person, 'first_name')
+      expect(check_new).to be false
+
+      person.update first_name: "Micheal"
+      person.update updated_at: DateTime.now.days_ago(2)
+      check_old = problem_checker.check_for_conflict(person, 'first_name')
+      expect(check_old).to be false
+    end
+
+    it "checks if new gathered information about a person is in conflict to already existing information" do
+      check_nil = problem_checker.check_for_information_conflict(person, 'Telegram')
+      expect(check_nil).to be false
+
+      info = person.informations.create(key: 'Telegram', value: '@hpi')
+      check = problem_checker.check_for_information_conflict(person, 'Telegram')
+      expect(check).to be true
+
+      info.update updated_at: DateTime.now.days_ago(2)
+      check_old = problem_checker.check_for_information_conflict(person, 'Telegram')
+      expect(check_old).to be false
+    end
   end
 
   context "when checking for empty fields" do
