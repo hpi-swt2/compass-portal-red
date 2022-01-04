35880d38991647efd091ece317b228cbde527481
cdfhalle <conradhalle@gmail.com>
2022-01-04 16:23:50 +0100

    feat: added the room_type attribute to the rooms detail page

--- spec/factories/rooms.rb
+++ spec/factories/rooms.rb
@@ -13,6 +13,7 @@ FactoryBot.define do
     tags do
       [(FactoryBot.create :tag), (FactoryBot.create :tag, :printer)]
     end
+    room_types { [(build :room_type, name: "Hörsal")] }
     chairs { [(build :chair, name: "EPIC")] }
     walls { [(build :wall)] }
     people { [(build :person, first_name: "Michael", last_name: "Perscheid")] }
