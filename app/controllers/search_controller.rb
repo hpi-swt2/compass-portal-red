49cb59afa1416036560a087c055defb4dc8111fe
genschelena <Elena.Gensch@student.hpi.uni-potsdam.de>
2022-01-07 11:58:15 +0100

    Adds search on word level for additional search results Additional search results are displayed below exact search results

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -2,9 +2,21 @@ class SearchController < ApplicationController
   def index
     return if params[:query].nil?
 
-    @results = Person.search(params[:query])
-    @results += Room.search(params[:query])
-    @results += Chair.search(params[:query])
+    queries = params[:query].scan(/[A-Za-z0-9]+/)
+    results = []
+
+    @exact_results = Person.search(params[:query])
+    @exact_results += Room.search(params[:query])
+    @exact_results += Chair.search(params[:query])
+
+    queries.each do |query|
+      results += Person.search(query)
+      results += Room.search(query)
+      results += Chair.search(query)
+    end
+
+    results = results.tally.sort_by { |record| record.second }.reverse!
+    @more_results = results.map {|record| record.first}.delete_if { |record| @exact_results.include? record }
     @params = params[:query]
   end
 
