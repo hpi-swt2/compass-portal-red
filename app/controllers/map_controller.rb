832938557118fe47ce616f2658f552067f1ac0bb
Franz Sauerwald <15076254+FranzSw@users.noreply.github.com>
2022-01-10 20:54:49 +0100

    WIP: PoC of select room card

--- app/controllers/map_controller.rb
+++ app/controllers/map_controller.rb
@@ -2,5 +2,10 @@ class MapController < ApplicationController
   def index
     # Map page, accessible without login
     @buildings = Building.all
+    @selected_room = Room.find(map_params[:room_id]) if map_params[:room_id].present?
+  end
+
+  def map_params
+    params.permit(:room_id)
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_03_115028) do
+ActiveRecord::Schema.define(version: 2022_01_03_132947) do
 
   create_table "buildings", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
@@ -95,6 +95,8 @@ ActiveRecord::Schema.define(version: 2022_01_03_115028) do
     t.integer "point_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "description"
+    t.string "name"
     t.index ["point_id"], name: "index_point_of_interests_on_point_id"
   end
 
