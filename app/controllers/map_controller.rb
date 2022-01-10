25a466c80e029da4bc127031c63f251a5f36c736
richardschiemenz <61618635+richardschiemenz@users.noreply.github.com>
2022-01-10 15:11:00 +0100

    132 detailpage prettify (#140)
    * feat: reworked detail-page to table
    
    * feat: reworked ui with bootstrap for css
    
    * merge: resolved wrong merge
    
    * refactor: autofix rubocop
    
    Co-authored-by: Morpheus <Zyranix@users.noreply.github.com>

--- app/controllers/map_controller.rb
+++ app/controllers/map_controller.rb
@@ -2,6 +2,6 @@ class MapController < ApplicationController
   def index
     # Map page, accessible without login
     @buildings = Building.all
-    @points_of_interest = PointOfInterest.all.map { |poi| poi.to_geojson}
+    @points_of_interest = PointOfInterest.all.map(&:to_geojson)
   end
 end
