0aacb795211922eeff38eb920eea85121aab497e
SebastianWagner2 <82543715+SebastianWagner2@users.noreply.github.com>
2022-01-12 14:38:04 +0100

    Write tests for import osm script
    Co-authored-by: benjamin-frost <bfrost.schule@gmail.com>

---
+++ app/helpers/osm_helper.rb
@@ -0,0 +1,25 @@
+require 'indoor_helper'
+require 'optparse'
+require 'building'
+require 'nokogiri'
+
+include IndoorHelper
+
+module OsmHelper
+  def parseOsm(xml_string)
+    doc = Nokogiri::XML(xml_string)
+    potential_rooms = doc.css("//way")
+    points = doc.css("//node")
+    building = Building.create
+    
+    points.each do |point|
+      build_point_from(point)
+    end
+    
+    potential_rooms.each do |room|
+      room.css("tag").each do |tag|
+        build_room_from(room, building) if tag["k"] == "indoor" && tag["v"] == "room"
+      end
+    end
+  end
+end
--- lib/import/import_osm.rb
+++ lib/import/import_osm.rb
@@ -1,21 +1,5 @@
-require 'indoor_helper'
-require 'optparse'
-require 'building'
-require 'nokogiri'
+require 'osm_helper'
 
-include IndoorHelper
+include OsmHelper
 
-doc = Nokogiri::XML(File.open("app/assets/data/#{ARGV.first}"))
-potential_rooms = doc.css("//way")
-points = doc.css("//node")
-building = Building.create
-
-points.each do |point|
-  build_point_from(point)
-end
-
-potential_rooms.each do |room|
-  room.css("tag").each do |tag|
-    build_room_from(room, building) if tag["k"] == "indoor" && tag["v"] == "room"
-  end
-end
+parseOsm(File.open("app/assets/data/#{ARGV.first}"))
