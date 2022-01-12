50798846f62fd23bf7f54800477bab458c8e97ea
Kirill Postnov <kirill.postnov@student.hpi.uni-potsdam.de>
2022-01-12 15:36:16 +0100

    Add tests

--- spec/features/map/access_map_page_spec.rb
+++ spec/features/map/access_map_page_spec.rb
@@ -14,4 +14,20 @@ RSpec.describe "Map Page", type: :feature do
     visit map_path
     expect(page).to have_css '.leaflet'
   end
+
+  it "renders a popup with a link if a room is selected" do
+    room = FactoryBot.create :room
+
+    visit map_path(room_id: 1)
+
+    expect(page).to have_css('.map-popup') 
+    expect(page).to have_link(:href => room_path(room)) 
+  end
+
+  it "renders no popup if no room is selected" do
+    visit map_path
+    expect(page).to have_no_css('.map-popup') 
+  end
 end
+
+
