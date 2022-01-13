75b80c9ec07e6ef122025d88afab035c9498b4af
leandermasopust <leander.masopust@gmail.com>
2022-01-13 15:49:31 +0100

    small fixes

--- spec/views/options/index.html.erb_spec.rb
+++ spec/views/options/index.html.erb_spec.rb
@@ -1,53 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe "options/index", type: :view do
-#  before do
-#    @person = Person.create!(first_name: "Rudolph", last_name: "Maffay", email: "pm@gmail.com", image: "/assets/images/people/csm_hpi-ite_valeska-maul_b80b438045.jpg")
-#  end
-#  it "does not render a 'log out' and
-#  'Edit personal Information' button when signed out" do
-#    # user_signed_in = false
-#    render "options/signed_out"
-#    expect(rendered).to match("Sign in")
-#  end
-#     it "renders a 'log out' and 'Edit personal Information' button when signed in" do
-#      user = User.create!(
-#           username: "Peter Maffay",
-#           password: "imakeyousexy",
-#           email: "peter.maffay@hpi.de"
-#         )
-#  
-#  
-#       render "options/signed_in", :locals => {:person => person, :user => user}
-#       expect(rendered).to match("Log out")
-#       expect(rendered).to match("Edit Personal Information")
-#  
-#     end
-#     it "renders a 'Contribute - Solve Data problems' and
-#     'Report a problem' button when signed in as non admin user" do
-#       user = User.create!(
-#           username: "Peter Maffay",
-#           isAdmin: false,
-#           password: "imakeyousexy",
-#           email: "peter.maffay@hpi.de"
-#         )
-#  
-#      render "options/signed_in", :locals => {:person => person, :user => user}
-#       expect(rendered).to match("Contribute - Solve Data Problems")
-#       expect(rendered).to match("Report a Problem")
-#  
-#     end
-#     it "renders a 'Show data tables' and
-#     'Data management' button when signed in as admin user" do
-#       user = User.create!(
-#           username: "Peter Maffay",
-#           isAdmin: true,
-#           password: "imakeyousexy",
-#           email: "peter.maffay@hpi.de"
-#         )
-#  
-#        render "options/signed_in", :locals => {:person => person, :user => user}
-#       expect(rendered).to match("Show data tables")
-#       expect(rendered).to match("Data Management")
-#     end
+    it "renders without an error" do
+        render
+        expect(rendered).to match("Sign in")
+    end
 end
