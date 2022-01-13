43e02ecfa976a2059334069f71e1228ef2bd8839
leandermasopust <leander.masopust@gmail.com>
2022-01-13 16:04:41 +0100

    fix linting issues

--- app/controllers/options_controller.rb
+++ app/controllers/options_controller.rb
@@ -1,13 +1,10 @@
 class OptionsController < ApplicationController
   def index
-    # Options page 
-    if user_signed_in? 
-     
-      user_id = current_user[:user_id ]
-      matched_people = Person.where(user_id: user_id)
-      if !matched_people.empty? 
-        @person = matched_people[0]
-      end
-    end
+    # Options page
+    return unless user_signed_in?
+
+    user_id = current_user[:user_id]
+    matched_people = Person.where(user_id: user_id)
+    @person = matched_people[0] unless matched_people.empty?
   end
 end
--- spec/views/options/index.html.erb_spec.rb
+++ spec/views/options/index.html.erb_spec.rb
@@ -1,8 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe "options/index", type: :view do
-    it "renders without an error" do
-        render
-        expect(rendered).to match("Sign in")
-    end
+  it "renders without an error" do
+    render
+    expect(rendered).to match("Sign in")
+  end
 end
