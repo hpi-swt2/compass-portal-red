2500a68cf351d0184411fc5cce48e9fdbeada318
Paul Sieben <67124476+PaulVII@users.noreply.github.com>
2022-01-13 18:54:03 +0100

    adress subject linting issue

--- spec/controllers/map_controller_spec.rb
+++ spec/controllers/map_controller_spec.rb
@@ -2,35 +2,35 @@ require 'rails_helper'
 
 RSpec.describe MapController, type: :controller do
 
-  subject { described_class.new }
+  subject(:map_controller) { described_class.new }
   describe "GET #url" do
     before do
-      subject.params[:overview] = 'false'
-      subject.params[:alternativs] = 'true'
-      subject.params[:steps] = 'true'
-      subject.params[:profile] = 'walking'
+      map_controller.params[:overview] = 'false'
+      map_controller.params[:alternativs] = 'true'
+      map_controller.params[:steps] = 'true'
+      map_controller.params[:profile] = 'walking'
       # main building to campus III
-      subject.params[:coordinates] = 
+      map_controller.params[:coordinates] = 
         '13p132964372634888%2C52p39388888702635;13p12498211860657%2C52p39511316914987' 
     end
 
     it 'returns the right url given the params' do
-      expect(subject.url).to eq URI "https://api.mapbox.com/directions/v5/mapbox/walking/13%2E132964372634888%2C52%2E39388888702635;13%2E12498211860657%2C52%2E39511316914987?access_token=#{ENV['MAPBOX_ACCESS_TOKEN']}&steps=true"
+      expect(map_controller.url).to eq URI "https://api.mapbox.com/directions/v5/mapbox/walking/13%2E132964372634888%2C52%2E39388888702635;13%2E12498211860657%2C52%2E39511316914987?access_token=#{ENV['MAPBOX_ACCESS_TOKEN']}&steps=true"
     end
 
     it 'doesn\' permit additional attributes return the right url given the params' do
-      subject.params[:forbidden] = 'value'
-      expect(subject.url).to eq URI "https://api.mapbox.com/directions/v5/mapbox/walking/13%2E132964372634888%2C52%2E39388888702635;13%2E12498211860657%2C52%2E39511316914987?access_token=#{ENV['MAPBOX_ACCESS_TOKEN']}&steps=true"
+      map_controller.params[:forbidden] = 'value'
+      expect(map_controller.url).to eq URI "https://api.mapbox.com/directions/v5/mapbox/walking/13%2E132964372634888%2C52%2E39388888702635;13%2E12498211860657%2C52%2E39511316914987?access_token=#{ENV['MAPBOX_ACCESS_TOKEN']}&steps=true"
     end
   end
 
   describe "GET #is_in_babelsberg" do
     it 'returns return true for points inside of babelsberg' do
-      expect(subject.is_in_babelsberg '13.2,52.4').to be_truthy
+      expect(map_controller.is_in_babelsberg('13.2,52.4')).to be_truthy
     end
 
     it 'returns return false for points outside of babelsberg' do
-      expect(subject.is_in_babelsberg '13.0,52.4').to be_falsey
+      expect(map_controller.is_in_babelsberg('13.0,52.4')).to be_falsey
     end
   end
 end
