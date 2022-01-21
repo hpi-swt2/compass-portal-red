f919b0477b63f5a4d60b4f9f42ce3d84737472f3
Paul Sieben <67124476+PaulVII@users.noreply.github.com>
2022-01-21 14:30:38 +0100

    fix: all points get replaced, even outside of coordinates

--- app/javascript/packs/router.js
+++ app/javascript/packs/router.js
@@ -1,8 +1,11 @@
 Router = L.Routing.OSRMv1.extend({
-    buildRouteUrl: function(waypoints, options){
-        // console.log(this)
+    buildRouteUrl: function (waypoints, options) {
+        waypoints.forEach(v => {
+            v.latLng.lat = String(v.latLng.lat).replaceAll('.', 'p')
+            v.latLng.lng = String(v.latLng.lng).replaceAll('.', 'p')
+        });
         url = L.Routing.OSRMv1.prototype.buildRouteUrl.call(this, waypoints, options);
-        url = url.replaceAll(',','%2C').replaceAll('.','p')
+        url = url.replaceAll(',', '%2C')
         return url
     }
 })
