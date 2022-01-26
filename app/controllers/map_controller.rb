e320c3ed919e904ff5a2d0235a0e0731e97a2dd3
Tom Richter <tomrichter99@gmail.com>
2022-01-26 14:31:02 +0100

    let mapcontroller inherit from search controller

--- app/controllers/map_controller.rb
+++ app/controllers/map_controller.rb
@@ -1,15 +1,15 @@
 require 'uri'
 require 'net/http'
 
-class MapController < ApplicationController
+class MapController < SearchController
   layout 'fullpage'
 
   def index
+    super
     # Map page, accessible without login
     @buildings = Building.all
     @points_of_interest = PointOfInterest.all.map(&:to_geojson)
     @selected_room = Room.find(map_params[:room_id]) if map_params[:room_id].present?
-    @params = params[:query]
   end
 
   def map_params
