78ffcce74b2f1ef5e482d443f73ac2ede9872531
AbdullatifGhajar <abdullatif.ghajar@gmail.com>
2022-01-26 16:36:49 +0100

    Sort more results
    Make related results less prioritized

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -3,13 +3,7 @@ class SearchController < ApplicationController
     return if params[:query].nil?
 
     @exact_results = add_results_for(params[:query])
-
-    words_in_query = params[:query].scan(/[A-Za-z0-9]+/)
-
-    @more_results = words_in_query.flat_map { |word| add_results_for(word) }
-    @more_results += @more_results.map(&:related_searchable_records).flatten
-    @more_results += @exact_results.map(&:related_searchable_records).flatten
-    @more_results = sort(@more_results, params[:query]) - @exact_results
+    @more_results = more_results(params, @exact_results)
 
     @params = params[:query]
   end
@@ -17,6 +11,14 @@ class SearchController < ApplicationController
   helper_method :index
 
   private
+
+  def more_results(params, exact_results)
+    words_in_query = params[:query].scan(/[A-Za-z0-9]+/)
+
+    more_results = words_in_query.flat_map { |word| add_results_for(word) }
+    related_results = (exact_results + more_results).map(&:related_searchable_records).flatten
+    sort(more_results + related_results, related_results, params[:query]) - exact_results
+  end
 
   def add_results_for(query)
     Person.search(query) +
@@ -37,14 +39,34 @@ class SearchController < ApplicationController
     prioritized_results
   end
 
-  def sort_by_frequency(array)
-    array = array.tally
-    array = array.sort_by(&:second).reverse!
-    array.map(&:first)
+  def compare
+    lambda { |tuple1, tuple2|
+      if tuple1.second == tuple2.second
+        tuple1.third <=> tuple2.third
+      else
+        tuple2.second <=> tuple1.second
+      end
+    }
   end
 
-  def sort(results, query)
-    results = sort_by_frequency(results)
+  def sort_by_frequency(primary_results, seconary_results)
+    primary_results_frequencies = primary_results.tally # [[A:4][B:4][C:1]]
+    seconary_results_frequencies = seconary_results.tally # [[A:3][B:2]]
+
+    results = [] # [[A:4:3][B:4:2][C:1:0]]
+    primary_results_frequencies.each do |key, value|
+      if seconary_results_frequencies.key?(key)
+        results.push([key, value, seconary_results_frequencies[key]])
+      else
+        results.push([key, value, 0])
+      end
+    end
+
+    results.sort(&compare).map(&:first) # [A B C]
+  end
+
+  def sort(more_results, related_results, query)
+    results = sort_by_frequency(more_results, related_results)
     sort_by_priority(results, query)
   end
 end
