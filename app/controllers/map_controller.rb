bbe316602895425a5ee5fea13bac162266db788b
Tom Richter <tomrichter99@gmail.com>
2022-01-26 13:23:50 +0100

    persist params on map

--- app/controllers/map_controller.rb
+++ app/controllers/map_controller.rb
@@ -9,6 +9,7 @@ class MapController < ApplicationController
     @buildings = Building.all
     @points_of_interest = PointOfInterest.all.map(&:to_geojson)
     @selected_room = Room.find(map_params[:room_id]) if map_params[:room_id].present?
+    @params = params[:query]
   end
 
   def map_params
