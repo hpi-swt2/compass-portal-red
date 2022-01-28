448b516a1cfe3119d167af5c2911b36a2dc351e8
Cedric Lorenz <37962195+cedric-lorenz@users.noreply.github.com>
2022-01-28 11:51:07 +0100

    Fix room types and tag search (#220)
    * add test for searching room without room_type
    
    * fix room_type & tag search by adding leftouterjoin
    
    Co-authored-by: Cedric Lorenz <cedric-lorenz@users.noreply.github.com>

--- features/step_definitions/room/room_steps.rb
+++ features/step_definitions/room/room_steps.rb
@@ -1,3 +1,7 @@
 When 'a room was created' do
   FactoryBot.create :room
 end
+
+When 'a room without a room type was created' do
+  FactoryBot.create :room, room_types: [], tags: [Tag.create(name: "quiet")], full_name: "H-E.43"
+end
