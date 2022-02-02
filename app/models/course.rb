bba7673a2dc330c4d49764ffc5579a816e30d653
Margarete <margarete.dippel@student.hpi.de>
2022-02-02 18:15:45 +0100

    create detail page for courses

--- app/models/course.rb
+++ app/models/course.rb
@@ -4,4 +4,11 @@ class Course < ApplicationRecord
   has_and_belongs_to_many :people
   has_many :course_times, dependent: :destroy
   belongs_to :room, optional: true
+  has_one_attached :image, dependent: :destroy
+
+  PLACEHOLDER_IMAGE_LINK = "placeholder_course.png".freeze
+
+  def image_or_placeholder
+    image.attached? ? image : PLACEHOLDER_IMAGE_LINK
+  end
 end
