83ad46cd2d0b0e00c6fadef3589b3768f8a780cd
Lukas Wenner <lukas.wenner@web.de>
2022-02-02 18:13:39 +0100

    feat: add course search

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,7 +1,24 @@
-class Course < ApplicationRecord
+# The model representing a course held at HPI
+class Course < SearchableRecord
   validates :name, presence: true
 
   has_and_belongs_to_many :people
   has_many :course_times, dependent: :destroy
   belongs_to :room, optional: true
+
+  def image_or_placeholder
+    "https://via.placeholder.com/512?text=Course"
+  end
+
+  def displayed_tags
+    [module_category]
+  end
+
+  def to_string
+    name
+  end
+
+  def self.searchable_attributes
+    %w[name module_category]
+  end
 end
