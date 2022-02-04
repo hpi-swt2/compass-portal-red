9c163c5a2232090ceac7ad00922c69d4eb27d12a
MartinPreiss <martinpreiss524@gmail.com>
2022-02-04 15:04:12 +0100

    rubocop friendly

--- app/models/searchable_record.rb
+++ app/models/searchable_record.rb
@@ -26,15 +26,18 @@ class SearchableRecord < ApplicationRecord
     "search-item-icon --#{self.class.name.downcase}"
   end
 
+  def self.search_string(query)
+    like_operator = ActiveRecord::Base.connection.adapter_name == "SQLite" ? "like" : "ilike"
+    attributes = searchable_attributes.map { |attribute| "#{attribute} " + like_operator + " '%#{query}%'" }
+    attributes.join(" or ")
+  end
+
   def self.search(query)
     join = left_outer_joins(searchable_relations)
     if query.strip.casecmp(name).zero?
       join.group(:id)
     else
-      like_operator = ActiveRecord::Base.connection.adapter_name=="SQLite" ? "like" : "ilike"
-      attributes = searchable_attributes.map { |attribute| "#{attribute} "+like_operator+" '%#{query}%'" }
-      search_string = attributes.join(" or ")
-      join.where(search_string).group(:id)
+      join.where(search_string(query)).group(:id)
     end
   end
 end
