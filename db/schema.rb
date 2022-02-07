8dcdc0d51dff2a8ed2a334587c8e0af83e4b791e
treyfel <58570309+treyfel@users.noreply.github.com>
2022-02-07 16:31:21 +0100

    update db

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,8 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_31_101826) do
+ActiveRecord::Schema.define(version: 2022_02_07_140303) do
+
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
