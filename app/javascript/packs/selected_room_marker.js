b8af459cd39d379b987e85f36f5fe26141d47c2c
DevSchmidtchen <contact@schmidtchen.me>
2022-02-08 09:42:55 +0100

    correct indentation

--- app/javascript/packs/selected_room_marker.js
+++ app/javascript/packs/selected_room_marker.js
@@ -3,13 +3,13 @@ const { indoorZoomLevel } = require("../constants");
 // Timeout to make sure that init of other code is done
 // TODO: Find nicer solution (e.g. JS CustomEvent)
 setTimeout(() => {
-    console.log('[SELECTED_ROOM] Selected room start');
-    const room = layers[`${selected_room_name}`];
-    // TODO: Add message?
-    if(!room) return;
+  console.log('[SELECTED_ROOM] Selected room start');
+  const room = layers[`${selected_room_name}`];
+  // TODO: Add message?
+  if(!room) return;
 
-    const coordinates = room.getBounds().getCenter();
-    L.marker(coordinates).addTo(mymap);
-    mymap.setView(coordinates, indoorZoomLevel + 1);
-    console.log('[SELECTED_ROOM] Selected room done');
+  const coordinates = room.getBounds().getCenter();
+  L.marker(coordinates).addTo(mymap);
+  mymap.setView(coordinates, indoorZoomLevel + 1);
+  console.log('[SELECTED_ROOM] Selected room done');
 }, 100);
